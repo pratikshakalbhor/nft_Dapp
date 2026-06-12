ca34ddd44c8d86a6efe29091c8a5fcaec23b8211
pratikshakalbhor <kalbhorpratiksha333@gmail.com>
2026-06-12 21:26:54 +0530

    Fix 'Buffer is not defined' error by adding polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Buffer } from 'buffer';
+window.Buffer = window.Buffer || Buffer;
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
